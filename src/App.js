dc80630b3ac3b502e21bf078d23d1a812c3c5334
emirtemiz0 <temizemir@outlook.com>
2021-09-23 12:04:10 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Todolist from "./Todolist";
+
+const App = () => {
+  return <Todolist />;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
